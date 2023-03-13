2a68e76f28a746c616d948085cbf8c5ca1f1c754
proximalbeet <97807056+proximalbeet@users.noreply.github.com>
2023-03-13 10:24:11 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGChar_PM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RPGCharacter _character = new RPGCharacter();
+        private Random _rng= new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            updateStats();
+            int d1 = RPGCharacter.RollDice(2, 20);
+            int d2 = RPGCharacter.RollDice(1000, 8);
+            MessageBox.Show(d1.ToString());
+            MessageBox.Show(d2.ToString());
+        }
+
+        private void buttonUpdateName_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Name = textBoxName.Text;
+            
+        }
+
+        private void buttonReroll_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Roll();
+            updateStats();
+            double odds = 0.5f;
+
+            _character.PartyMembers.Clear();
+            foreach (ListBoxItem i in ListPotentialMembers.Items) 
+            { if (_rng.NextDouble() > odds)
+                {
+                    RPGCharacter r = new RPGCharacter()
+                    {
+                        Name = i.Content.ToString()
+                    };
+                    _character.PartyMembers.Add(r);
+                }
+            }
+            ListPartyNumbers.Items.Clear();
+            foreach (RPGCharacter r in _character.PartyMembers) 
+            { 
+              ListBoxItem i = new ListBoxItem();
+                i.Content = $"{r.Name} STR: {r.Strength} INT: {r.Intelligence}"; 
+              ListPartyNumbers.Items.Add(i);
+            }
+        }
+
+        
+        private void updateStats()
+        {
+            textStrength.Text = _character.Strength.ToString();
+            textIntelligence.Text = _character.Intelligence.ToString();
+            textDexterity.Text = _character.Dexterity.ToString();
+            textStamina.Text = _character.Stamina.ToString();
+            textWisdom.Text = _character.Wisdom.ToString();
+            textCharisma.Text = _character.Charisma.ToString();
+        }
+
+       
+    }
+}
---
+++ RPGCharacter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGChar_PM
+{
+    internal class RPGCharacter
+    {
+        // Fields
+        private int _charisma;
+        private int _dexterity;
+        private int _intelligence;
+        private int _stamina;
+        private int _strength;
+        private int _wisdom;
+        private int _maxCharisma = 20;
+        private int _maxDexterity = 20;
+        private int _maxIntelligence = 20;
+        private int _maxStamina = 20;
+        private int _maxStrength = 20;
+        private int _maxWisdom = 20;
+        private List<RPGCharacter> _partyMembers = new List<RPGCharacter>();
+
+        private CharacterClasses _characterClass = CharacterClasses.None;
+        private Random _rng = new Random();
+
+        #region Properties
+        public string Name { get; set; }
+        public long XP { get; set; }
+        public int Level { get; set; }
+        public CharacterClasses CharacterClass
+        {
+            get { return _characterClass; }
+            set { _characterClass = value; }
+        }
+        public List<RPGCharacter> PartyMembers
+        {
+            get { return _partyMembers; }
+            set { _partyMembers = value; }
+        }
+
+        public int Charisma { get { return _charisma; } }
+        public int Dexterity { get { return _dexterity; } }
+        public int Intelligence { get { return _intelligence; } }
+        public int Stamina { get { return _stamina; } }
+        public int Strength { get { return _strength; } }
+        public int Wisdom { get { return _wisdom; } }
+        #endregion
+
+        public RPGCharacter()
+        {
+            Roll();
+        }
+
+        public void Roll()
+        {
+            _charisma = _rng.Next(1, _maxCharisma + 1);
+            _dexterity = _rng.Next(1, _maxDexterity + 1);
+            _intelligence = _rng.Next(1, _maxIntelligence + 1); ;
+            _stamina = _rng.Next(1, _maxStamina + 1);
+            _strength = _rng.Next(1, _maxStrength + 1);
+            _wisdom = _rng.Next(1, _maxWisdom + 1);
+        }
+
+        public static int RollDice(int numberOfDice, int numberOfSides) 
+        {
+            Random r = new Random();
+            int total = 0;
+
+            for (int i = 0; i < numberOfDice; i++) 
+            { 
+             total+= r.Next(1, numberOfSides);
+            }
+            return total;
+        }
+            
+
+    }
+
+    public enum CharacterClasses
+    {
+        None = 0,
+        Ranger,
+        Mage,
+        Rogue,
+        Bard,
+        Goliath,
+        Paladin
+    }
+}
